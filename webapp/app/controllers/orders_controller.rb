a81c225ade182878ecf39d72ebc23ea748a57a7c
Delvison Castillo <delvisoncastillo@gmail.com>
2016-08-29 19:53:04 -0400

    rails app successfully sending post data to restful api

--- webapp/app/controllers/orders_controller.rb
+++ webapp/app/controllers/orders_controller.rb
@@ -1,8 +1,25 @@
+require 'httparty'
+require "net/http"
+
 class OrdersController < ApplicationController
   def index
+      url = 'http://127.0.0.1:8080/api/orders'
+      api_response = HTTParty.get(url)
+      @all_orders = api_response.parsed_response
   end
 
   def new
+      @order = Order.new
+  end
+
+  def create
+      @order = params[:order]
+      url = 'http://127.0.0.1:8080/api/orders'
+      api_response = HTTParty.post(url,
+        :body => @order)
+      puts api_response.body
+    #  TODO: error handling
+      redirect_to orders_path
   end
 
   def show
